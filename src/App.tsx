8a7b08e0665161f19434784995438205490d9138
mbarnesdev <martinbarnes.dev@gmail.com>
2023-01-29 18:44:53 +0000

    remove a11y

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Navigation, Home, Form, Todo } from '@/components';
 
 const App = () => {
   return (
-    <>
+    <Fragment>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<Form />} />
         <Route path="/todo" element={<Todo />} />
       </Routes>
-    </>
+    </Fragment>
   );
 };
 
--- src/hooks/useZodForm.ts
+++ src/hooks/useZodForm.ts
@@ -1,7 +1,9 @@
+/* https://www.brendonovich.dev/blog/the-ultimate-form-abstraction */
+
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import type { TypeOf, ZodSchema } from 'zod';
 import type { UseFormProps } from 'react-hook-form';
+import type { TypeOf, ZodSchema } from 'zod';
 
 interface UseZodFormProps<Z extends ZodSchema>
   extends Exclude<UseFormProps<TypeOf<Z>>, 'resolver'> {
